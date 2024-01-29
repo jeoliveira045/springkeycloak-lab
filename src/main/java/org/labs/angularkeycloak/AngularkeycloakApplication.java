72fba4755400e4a179f5249eb0c29cc622b0ce07
jean.oliveira <jean.oliveira@tre-pa.jus.br>
2024-01-29 17:23:23 -0300

    Criação do security config e do logouthandler

--- src/main/java/org/labs/angularkeycloak/AngularkeycloakApplication.java
+++ src/main/java/org/labs/angularkeycloak/AngularkeycloakApplication.java
@@ -2,6 +2,8 @@ package org.labs.angularkeycloak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -12,6 +14,10 @@ public class AngularkeycloakApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AngularkeycloakApplication.class, args);
+    }
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
 
 }
--- src/main/java/org/labs/angularkeycloak/rest/AuthorRest.java
+++ src/main/java/org/labs/angularkeycloak/rest/AuthorRest.java
@@ -1,9 +1,22 @@
 package org.labs.angularkeycloak.rest;
 
+import org.labs.angularkeycloak.entities.Author;
+import org.labs.angularkeycloak.repository.AuthorRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping
+@RequestMapping("/author")
 public class AuthorRest {
+
+    private AuthorRepository repository;
+
+    @GetMapping
+    public String findAll(){
+        return "<h1>Todos os autores</h1>";
+    }
 }
